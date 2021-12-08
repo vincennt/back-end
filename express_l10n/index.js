f1cce871f4f474ae87034e2c4b167737eab4c439
Vincent <vins@Pimentas-MacBook-Air.local>
2021-12-08 10:34:22 +0100

    exo done

--- express_l10n/index.js
+++ express_l10n/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const data = require("./translations.json");
-const img = require("./public");
 
 const port = 5000;
 console.log(data.fr);
@@ -10,10 +9,11 @@ const engine = require("express-handlebars").engine;
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.use(express.urlencoded({ extended: true }));
-app.post("/public/:img", (req, res) => {});
 
 app.get("/:lang?", (req, res) => {
   const { lang } = req.params;
+
+  app.use(express.static("public"));
 
   res.render("home", {
     pageTitle: data[lang].pageTitle,
